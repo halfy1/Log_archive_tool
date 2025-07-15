e0042a69dfb0016ad0d5510cb4a1d62136a11572
halfy1 <54471372+halfy1@users.noreply.github.com>
2025-07-15 23:08:58 +0300

    Доделан архиватор и логгирование архивов

--- archive.py
+++ archive.py
@@ -2,6 +2,8 @@ import argparse
 import os
 import sys
 from datetime import datetime
+import tarfile
+
 parser = argparse.ArgumentParser(description="Log archive tool")
 parser.add_argument("indir", help="directory to logs")
 args = parser.parse_args()
@@ -19,3 +21,17 @@ archive_path = os.path.join("archives", archive_name)
 
 print(f"Имя архива {archive_name}")
 print(f"Путь для архива {archive_path}")
+
+os.makedirs("archives", exist_ok=True)
+
+with tarfile.open(archive_path, "w:gz") as tar:
+    tar.add(log_dir, arcname=os.path.basename(log_dir))
+
+os.makedirs("logs", exist_ok=True)
+
+human_time=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+log_line = f"[{human_time}] Архив создан: {archive_path} из {log_dir}\n"
+
+with open("logs/archive_log.txt", "a") as log_file:
+    log_file.write(log_line)
+
